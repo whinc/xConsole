47ecff02eb10d4a2bfe820952d70008db68feeec
whincwu <whincwu@webank.com>
2017-10-17 17:42:36 +0800

    Add UI styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
+import {Tabs, Tab} from 'material-ui/Tabs'
 import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      value: 'Console',
+      logBuffer: []
+    }
+
+    const originLog = console.log
+    window.console.log = (...args) => {
+      const logBuffer = this.state.logBuffer
+      logBuffer.push(args.join(''))
+      this.setState({ logBuffer })
+      originLog(...args)
+    }
+  }
+
   render() {
     return (
-      <div>
-        <img className='App-logo App-logo-spin' src={logo} />
-        <p className='App-intro'>Hello World</p>
-      </div>
+      <Tabs value={this.state.value} style={{marginTop: '40vh'}} onChange={(value) => this.setState({value})}>
+        <Tab label='Console' value='Console'>
+          <div style={{overflow: 'scroll', height: '40vh'}}>
+            <span>Console</span>
+            {this.state.logBuffer.map((value, index) => <p key={index}>{value}</p>)}
+          </div>
+        </Tab>
+        <Tab label='Network' value='Network'>
+          <span>Network</span>
+        </Tab>
+      </Tabs>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+var entry = document.createElement('div')
+entry.style.cssText = `
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+`
+ReactDOM.render(
+  <MuiThemeProvider>
+    <RaisedButton label='xConsole' primary onClick={showPanel} />
+  </MuiThemeProvider>
+  ,entry)
+
+window.addEventListener('load', function () {
+  document.body.appendChild(entry)
+})
+
+var panel = null
+
+function showPanel () {
+  if (!panel) {
+    panel = document.createElement('div')
+    panel.style.cssText = `
+      position: absolute;
+      top: 0px;
+      bottom: 0px;
+      left: 0px;
+      right: 0px;
+    `
+    ReactDOM.render(
+      <MuiThemeProvider>
+        <App onClose={() => document.body.removeChild(panel)} />
+      </MuiThemeProvider>
+    , panel)
+  }
+  document.body.appendChild(panel)
+}
