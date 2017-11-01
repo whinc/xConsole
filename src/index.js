443e81cb5a74cc8c80a46c3f00ca69a195755c65
whincwu <whincwu@webank.com>
2017-11-01 16:34:12 +0800

    Remove css library 'bulma'

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import XConsole from './XConsole'
 
 if (!window.XConsole) {
   window.XConsole = XConsole
-  addCSSLink('https://unpkg.com/bulma@0.6/css/bulma.css')
-}
-
-// development mode
-if (window.location.hostname === 'localhost') {
-  new XConsole()
 }
 
 export default XConsole
-
-function addCSSLink (url) {
-  var link = document.createElement('link')
-  link.setAttribute('rel', 'stylesheet')
-  link.setAttribute('href', url)
-  document.head.appendChild(link)
-}
