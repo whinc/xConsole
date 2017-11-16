0bfb13f64456cddd76f75ddba6bb045bdccb09b1
whincwu <whincwu@163.com>
2017-11-17 00:34:50 +0800

    feat(ConsolePlugin): display different summary in console output base on fold status of printed object

--- src/plugins/ConsolePlugin/TextBlock.js
+++ src/plugins/ConsolePlugin/TextBlock.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import isObject from 'lodash.isobject'
-// import isArray from 'lodash.isarray'
+import isArray from 'lodash.isarray'
 import isBoolan from 'lodash.isboolean'
 import isString from 'lodash.isstring'
 import isNumber from 'lodash.isnumber'
@@ -12,11 +12,15 @@ import './TextBlock.css'
 export default class TextBlock extends React.Component {
   static PropTypes = {
     value: PropTypes.any.isRequired,
-    sumary: PropTypes.bool
+    isSummary: PropTypes.bool,
+    isRoot: PropTypes.bool
   }
 
   static defaultProps = {
-    sumary: true
+    // If current block display as a summary message or a detail list
+    isSummary: true,
+    // If current <TextBlock> is the root block
+    isRoot: true
   }
 
   constructor () {
@@ -34,8 +38,58 @@ export default class TextBlock extends React.Component {
     })
   }
 
+  createArraySummary (input) {
+    let r = input.reduce((r, value) => {
+      return r + `${this.createSummary(value, false, false)}, `
+    }, '')
+    r = r.substring(0, r.length - 2)  // remove last comma and space
+    return `[${r}]`
+  }
+
+  createObjectSummary (input) {
+    let r = Object.keys(input).reduce((r, key) => {
+      const value = input[key]
+      return r + `${key}: ${this.createSummary(value, false, false)}, `
+    }, '')
+    // remove last comma and space
+    r = r.substring(0, r.length - 2)
+    return `{${r}}`
+  }
+
+  /**
+   * Create summary of the input value based on it's type, foled status and is root
+   * @param {any} input
+   * @param {boolean} isFolded
+   * @param {boolean} isRoot
+   */
+  createSummary (input, isFolded = true, isRoot = true) {
+    if (isArray(input)) {
+      if (isRoot) {
+        return this.createArraySummary(input)
+      } else {
+        if (isFolded) { // If array is foled, display a summary of array
+          return this.createArraySummary(input)
+        } else { // If array is unfoled, display the type and length of array
+          return `Array(${input.length})`
+        }
+      }
+    } else if (isObject(input)) {
+      if (isRoot) {
+        return this.createObjectSummary(input)
+      } else {
+        if (isFolded) { // If object is foled, display a summary of object
+          return this.createObjectSummary(input)
+        } else {  // If object is unfoled, display nothing
+          return '{...}'
+        }
+      }
+    } else {
+      return String(input)
+    }
+  }
+
   render () {
-    const {value: input, sumary} = this.props
+    const {value: input, isSummary, isRoot} = this.props
     const {isFolded} = this.state
 
     switch (true) {
@@ -52,21 +106,21 @@ export default class TextBlock extends React.Component {
       case isObject(input):
         /*
          * If input is 'object' it can display with two mode:
-         * 1. show one line sumary
+         * 1. show one line summary
          * 2. show mutil-lines, each line display a key-value propperty of the input
          *
-         * AAAA (sumary === true)
+         * AAAA (summary === true)
          *
-         * AAAA (sumary === false)
+         * AAAA (summary === false)
          *   BB: CC
          *   DD: EE
          */
-        if (sumary) {
+        if (isSummary) {
           return (
             <span className='TextBlock TextBlock--vertical'>
-              <span onClick={this.toggleFoldStatus}>{JSON.stringify(input)}</span>
+              <span onClick={this.toggleFoldStatus}>&nbsp;{this.createSummary(input, isFolded, isRoot)}</span>
               <span>
-                {isFolded ? null : <TextBlock value={input} sumary={false} /> }
+                {!isFolded && <TextBlock value={input} isSummary={false} isRoot={false} /> }
               </span>
             </span>
           )
@@ -79,7 +133,7 @@ export default class TextBlock extends React.Component {
                     <span>{key}</span>
                     <span>:</span>
                     <span>
-                      <TextBlock value={input[key]} sumary />
+                      <TextBlock value={input[key]} isSummary isRoot={false} />
                     </span>
                   </span>
                 )
