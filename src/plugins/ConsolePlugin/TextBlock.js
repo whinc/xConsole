761ac0bff2d9077f575a5891c6649ac9610d68a3
whincwu <whincwu@163.com>
2017-11-18 16:46:55 +0800

    refactor(ConsolePlugin): simple the implementation of create summary in TextBlock component

--- src/plugins/ConsolePlugin/TextBlock.js
+++ src/plugins/ConsolePlugin/TextBlock.js
@@ -7,6 +7,7 @@ import isString from 'lodash.isstring'
 import isNumber from 'lodash.isnumber'
 import isNull from 'lodash.isnull'
 import isUndefined from 'lodash.isundefined'
+import isFunction from 'lodash.isfunction'
 import './TextBlock.css'
 
 export default class TextBlock extends React.Component {
@@ -19,10 +20,6 @@ export default class TextBlock extends React.Component {
   static defaultProps = {
     name: '',
     indentSize: 0
-    // If current block display as a summary message or a detail list
-    // isSummary: true,
-    // If current <TextBlock> is the root block
-    // isRoot: true
   }
 
   constructor () {
@@ -40,70 +37,51 @@ export default class TextBlock extends React.Component {
     })
   }
 
-  // e.g. [1, [2, 3], 4] --> [1, Array(2), 4]
-  createArraySummary (input) {
-    let r = input.reduce((r, value) => {
-      return r + `${this.createSummary(value, false, false)}, `
-    }, '')
-    r = r.substring(0, r.length - 2)  // remove last comma and space
-    return `[${r}]`
-  }
-
-  // e.g. {a: 1, b: {c: 1}, d: 3} --> {a: 1, b: {...}, d: 3}
-  createObjectSummary (input) {
-    let r = Object.keys(input).reduce((r, key) => {
-      const value = input[key]
-      return r + `${key}: ${this.createSummary(value, false, false)}, `
-    }, '')
-    // remove last comma and space
-    r = r.substring(0, r.length - 2)
-    return `{${r}}`
-  }
-
   /**
-   * Create summary of the input value based on value type and the current status of TextBlock
-   * @param {any} input
-   * @param {boolean} isFolded
-   * @param {boolean} isRoot
+   * Create the summary of passed in value
+   * @param {any} value passwd in value
+   * @param {sting = ''} name the name of value
+   * @param {boolean} isFolded the value whether folded or not
    */
-  createSummary (input, isFolded = true, isRoot = true) {
-    if (isString(input)) {
-      return `"${input}"`
-    } else if (isNumber(input)) {
-      return String(input)
-    } else if (isBoolan(input)) {
-      return String(input)
-    } else if (isNull(input)) {
-      return String(input)
-    } else if (isUndefined(input)) {
-      return String(input)
-    } else if (typeof input === 'function') {
-      // TODO:
-      // 1. replace the first 'function' with 'f'
-      // 2. use isFunction() to adjudge the function type
-      return `"${String(input)}"`
-    } else if (isArray(input)) {
-      if (isRoot) {
-        return this.createArraySummary(input)
+  createSummary (value, name = '', isFolded = true) {
+    if (isString(value)) {
+      return `"${value}"`
+    } else if (isNumber(value)) {
+      return String(value)
+    } else if (isBoolan(value)) {
+      return String(value)
+    } else if (isNull(value)) {
+      return String(value)
+    } else if (isUndefined(value)) {
+      return String(value)
+    } else if (isFunction(value)) {
+      const summary = String(value).replace(/function/, 'f')
+      return `${summary}`
+    } else if (isArray(value)) {  // e.g. [1, [2, 3], 4] --> [1, Array(2), 4]
+      if (!name || isFolded) {
+        let summary = value.reduce((s, v, k) => {
+          return s + `${this.createSummary(v, k, false)}, `
+        }, '')
+        // remove last comma and space
+        summary = summary.substring(0, summary.length - 2)
+        return `[${summary}]`
       } else {
-        if (isFolded) { // If array is foled, display a summary of array
-          return this.createArraySummary(input)
-        } else { // If array is unfoled, display the type and length of array
-          return `Array(${input.length})`
-        }
+        return `Array(${value.length})`
       }
-    } else if (isObject(input)) {
-      if (isRoot) {
-        return this.createObjectSummary(input)
+    } else if (isObject(value)) { // e.g. {a: 1, b: {c: 1}, d: 3} --> {a: 1, b: {...}, d: 3}
+      if (!name || isFolded) {
+        let summary = Object.keys(value).reduce((s, k) => {
+          const v = value[k]
+          return s + `${k}: ${this.createSummary(v, k, false)}, `
+        }, '')
+        // remove last comma and space
+        summary = summary.substring(0, summary.length - 2)
+        return `{${summary}}`
       } else {
-        if (isFolded) { // If object is foled, display a summary of object
-          return this.createObjectSummary(input)
-        } else {  // If object is unfoled, display nothing
-          return '{...}'
-        }
+        return '{...}'
       }
     } else {
-      return String(input)
+      return String(value)
     }
   }
 
@@ -122,7 +100,7 @@ export default class TextBlock extends React.Component {
             {indentSize > 0 && <span className='TextBlock__indent'>{' '.repeat(indentSize)}</span>}
             {name && <span>{name}</span>}
             {name && <span>{': '}</span>}
-            <span>{this.createSummary(input)}</span>
+            <span>{this.createSummary(input, name, true)}</span>
           </span>
         )
       case isObject(input):
@@ -132,7 +110,7 @@ export default class TextBlock extends React.Component {
               {indentSize > 0 && <span className='TextBlock__indent'>{' '.repeat(indentSize)}</span>}
               {name && <span>{name}</span>}
               {name && <span>{': '}</span>}
-              <span onClick={this.toggleFoldStatus}>{this.createSummary(input)}</span>
+              <span onClick={this.toggleFoldStatus}>{this.createSummary(input, name, isFolded)}</span>
             </span>
             {!isFolded && Object.keys(input).map(key =>
               <TextBlock
