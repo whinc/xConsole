5d09eca7a70867e6d7046b1e8583d4c24146fedd
whincwu <whincwu@163.com>
2017-11-25 23:59:16 +0800

    feat: change generated js file name to same with the main field in package.json

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -66,7 +66,7 @@ module.exports = {
     // Generated JS file names (with nested folders).
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
-    filename: `${pkgName}-${pkgVersion}.js`,
+    filename: `${pkgName}.min.js`,
     chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
